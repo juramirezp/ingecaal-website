d4b8191c5824ea4a17eb0e11b06c2dfe6facfced
Juan Ramirez <juramirezp@Mac-mini-de-Juan.local>
2025-07-21 18:11:40 -0400

    front

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    icon({
+      include: {
+        solar: ["*"], // (Default) Loads entire Material Design Icon set
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
